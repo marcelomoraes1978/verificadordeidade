b833e94f0b66790b2177f6a31a98b1e74e5bc168
Marcelo Moraes <112552958+marcelomoraes1978@users.noreply.github.com>
2024-02-24 17:56:47 -0300

    arquivos do verificador

---
+++ script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os valores digitados e tente novamente!!!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'crianca-h.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-h.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-h.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-h.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'crianca-m.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-m.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos que você é um ${genero} e tem ${idade} anos!!!`
+        res.appendChild(img)
+    }
+}
